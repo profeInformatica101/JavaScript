55cf950965b054c98017a702aa363c6de4c67d36
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-01-12 00:01:27 +0100

    Create ej1_listas.js

---
+++ ej1_listas.js
@@ -0,0 +1,19 @@
+    // Crea una lista de números pares del 2 al 20 utilizando un bucle while.
+    
+    // Crea una lista de comida y utiliza un array literal para agregar varios platos a la lista.
+    
+    // Agrega un elemento al principio de la lista de comida que creaste en el ejercicio anterior.
+    
+    // Accede al segundo elemento de la lista de comida y cambia su valor por un plato diferente.
+    
+    // Recorre la lista de comida utilizando un bucle while y muestra cada elemento en la consola.
+    
+    // Crea una función que reciba una lista como parámetro y devuelva el último elemento de la lista.
+    
+    // Crea una función que reciba una lista y un elemento como parámetros y agregue el elemento a la lista si no se encuentra en ella.
+    
+    // Crea una función que reciba una lista como parámetro y devuelva una nueva lista con los elementos de la lista original en orden inverso.
+    
+    // Crea una función que reciba una lista de números como parámetro y devuelva la suma de todos los números de la lista.
+    
+    // Crea una función que reciba una lista de números como parámetro y devuelva la media de todos los números de la lista.
