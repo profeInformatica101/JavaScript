9d3e8a169b6c92825a5ac955aa8b297576fccc81
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-03-13 10:03:34 +0100

    Create ej2_funciones.js

---
+++ ej2_funciones.js
@@ -0,0 +1,9 @@
+//Crear una función que tome una lista de números y devuelva la suma de todos ellos.
+//Crear una función que tome dos números y devuelva el mayor de ellos.
+//Crear una función que tome dos números y devuelva el menor de ellos.
+//Crear una función que tome un número y devuelva verdadero si es par, falso si es impar.
+//Crear una función que tome una lista de números y devuelva el promedio de todos ellos.
+//Crear una función que tome una lista de números y devuelva el número más grande de la lista.
+//Crear una función que tome una lista de números y devuelva el número más pequeño de la lista.
+/* Crea una función que tome dos parámetros de entrada, una lista de números y un número.
+La función tiene que devolver true o false si el número se encuentra en la lista. */
